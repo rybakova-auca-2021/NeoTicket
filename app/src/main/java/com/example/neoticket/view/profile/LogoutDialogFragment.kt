5d6839e525178b36b1a43aff55775d3a800a5845
rybakova-auca-2021 <rybakova_k@auca.kg>
2024-01-26 20:23:34 +0600

    setup logout

--- app/src/main/java/com/example/neoticket/view/profile/LogoutDialogFragment.kt
+++ app/src/main/java/com/example/neoticket/view/profile/LogoutDialogFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.neoticket.R
+import com.example.neoticket.Utils.Util
 import com.example.neoticket.databinding.FragmentLogoutDialogBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -18,6 +19,21 @@ class LogoutDialogFragment : BottomSheetDialogFragment() {
     ): View? {
         binding = FragmentLogoutDialogBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        binding.btnCancel.setOnClickListener {
+            dismiss()
+        }
+        binding.btnLogout.setOnClickListener {
+            Util.token = null
+            dismiss()
+        }
     }
 
 }
