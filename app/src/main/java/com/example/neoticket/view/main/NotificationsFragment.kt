a3a6cb8b1ff340dad74ccecf4429e706eff0e498
rybakova-auca-2021 <rybakova_k@auca.kg>
2024-01-27 16:53:22 +0600

    created ui for notification pages

---
+++ app/src/main/java/com/example/neoticket/view/main/NotificationsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.neoticket.view.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.neoticket.R
+import com.example.neoticket.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+    private lateinit var binding: FragmentNotificationsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
